93a7749fb3928db82c10f6a6072c2dd5de092cb4
Захаров Денис <zdv.work@yandex.ru>
2024-05-10 19:09:45 +0600

    добавил вьюшку для пустого списка

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
-import { SORT_ITEMS } from './const.js';
 import { render } from './framework/render.js';
 import { generateFilter } from './mock/filter.js';
 import EventsModel from './model/events-model.js';
 import EventListPresenter from './presenter/events-presenter.js';
 import FilterView from './view/filter-view.js';
-import SortView from './view/sort-view.js';
 
 const controlFiltersElement = document.querySelector('.trip-controls__filters');
 const tripEventsElement = document.querySelector('.trip-events');
@@ -14,7 +12,5 @@ const eventListPresenter = new EventListPresenter({
   eventsModel
 });
 const filters = generateFilter(eventsModel.events);
-console.log(filters);
 render(new FilterView(filters), controlFiltersElement);
-render(new SortView(SORT_ITEMS), tripEventsElement);
 eventListPresenter.init();
